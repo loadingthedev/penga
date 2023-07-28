ee1b23cacd623d7f34b1572b80cb6669e8c34b89
loadingthedev <loadingthedev@gmail.com>
2023-07-28 15:54:37 +0530

    header overflow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,25 +10,39 @@ const Header = () => {
     <header className="h-[4.5rem] flex gap-6 items-center justify-between">
       <div className="flex items-center gap-5">
         <BiMenuAltLeft className="text-white cursor-pointer text-4xl" />
-        <Link href="/">
+        <Link href="/" className="hidden md:block">
           <div className="relative flex w-36 h-8">
             <Image
               src="/logo.webp"
               alt="logo"
               width={250}
               height={100}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="object-cover"
+              className="object-contain"
               quality={1}
               priority
             />
           </div>
         </Link>
+        <Link href="/" className="md:hidden">
+          <div className="relative w-24">
+            <Image
+              src="/logo.webp"
+              alt="logo"
+              width={250}
+              height={100}
+              priority
+            />
+          </div>
+        </Link>
       </div>
-      <div className="flex items-center gap-4 md:gap-3 ml-auto sm:ml-0">
+
+      <div className="flex items-center gap-2 md:gap-3 ml-auto sm:ml-0">
         <AiOutlineSearch className="text-2xl cursor-pointer shrink-0" />
         <ModeToggle />
-        <Button className="bg-[#2858d9] text-white px-5 py-[6px] hover:bg-[#2858d9] hover:opacity-80 transition-all">
+        <Button
+          size="sm"
+          className="bg-[#2858d9] text-white rounded-md hover:bg-[#2858d9] hover:opacity-80 transition-all"
+        >
           Login
         </Button>
       </div>
