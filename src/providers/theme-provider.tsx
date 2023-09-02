e57e756de5055613580cf3d85d5b165ecd300b95
loadingthedev <loadingthedev@gmail.com>
2023-09-02 16:29:23 +0530

    feat: smooth scroll

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -5,5 +5,12 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  React.useEffect(() => {
+    (async () => {
+      // @ts-ignore
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const scroll = new LocomotiveScroll({});
+    })();
+  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
